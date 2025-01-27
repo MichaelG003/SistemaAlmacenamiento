3bb2e3a6e7bde9d375546e4b0f107a8697e1d111
MichaelG003 <mickeyorihuela00@gmail.com>
2025-01-27 16:28:01 -0500

    Cambios pequeños

--- obj/Debug/net8.0/SistemaAlmacenamiento.AssemblyInfo.cs
+++ obj/Debug/net8.0/SistemaAlmacenamiento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaAlmacenamiento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b563fe93fe56cb3f4f336aebb1732fa7d5764fe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+782e9f9ceb74b3b7cf654005eda598b5e74ad308")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaAlmacenamiento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaAlmacenamiento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
